8b5095205c42ea6031c81f42b5f3b192536b70ff
Egor <petr616@yandex.ru>
2020-10-14 18:54:14 +0300

    Они говорят тебе, в чем твоя победа. Это ложь. Это фальшь. Подлинная победа в том, чтобы взять и пробить головой небо. Поменять правила их игры.

--- ViewModel.cs
+++ ViewModel.cs
@@ -25,7 +25,15 @@ namespace MM_IdealGas
 		private PhysFuncs _world;
 
 
-		public int CountParticles { get; set; } = 50;
+		private double deltaT = 1.0;
+
+		private double margin = 0.9;
+
+		private double maxU0 = 1.0;
+		private double r1 = 1.1;
+		private double r2 = 1.8;
+		private double mass = 39.948;
+		private int countParticles = 50;
 		public ICommand Generate { get; set; }
 		public ICommand Start { get; set; }
 		private ObservableCollection<Particle> _particles;
@@ -35,8 +43,8 @@ namespace MM_IdealGas
 
 			set
 			{
-			_particles=value;
-			OnPropertyChanged(nameof(Particles));
+				_particles = value;
+				OnPropertyChanged(nameof(Particles));
 			}
 		}
 		public ViewModel()
@@ -45,19 +53,19 @@ namespace MM_IdealGas
 			_world.SetMargin(Margin);
 			_world.SetParticlesQuantity(CountParticles);
 			_world.SetMaxInitVel(MaxU0);
-			_world.SetR1R2(R1,R2);
+			_world.SetR1R2(R1, R2);
 			_world.SetMass(Mass);
 			_world.SetTimeParams(DeltaT, TCounts);
 			_world.GenerateInitState();
 			Particles = _world.GetParticles();
-		
+
 			Generate = new RelayCommand(o =>
 			{
 				_world = new PhysFuncs();
 				_world.SetMargin(Margin);
 				_world.SetParticlesQuantity(CountParticles);
 				_world.SetMaxInitVel(MaxU0);
-				_world.SetR1R2(R1,R2);
+				_world.SetR1R2(R1, R2);
 				_world.SetMass(Mass);
 				_world.SetTimeParams(DeltaT, TCounts);
 				_world.GenerateInitState();
@@ -90,14 +98,72 @@ namespace MM_IdealGas
 			_world.DoStep();
 			Particles = _world.GetParticles();
 		}
-		
-		public double Margin { get; set; } = 0.9;
 
-		public double MaxU0 { get; set; } = 1.0;
-		public double R1 { get; set; } = 1.1;
-		public double R2 { get; set; } = 1.8;
-		public double Mass { get; set; } = 39.948;
-		public double DeltaT { get; set; } = 1.0;
+		public double Margin
+		{
+			get => margin;
+			set
+			{
+				margin = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public double MaxU0
+		{
+			get => maxU0;
+			set
+			{
+				maxU0 = value;
+				OnPropertyChanged();
+			}
+		}
+		public double R1
+		{
+			get => r1;
+			set
+			{
+				r1 = value;
+				OnPropertyChanged();
+			}
+		}
+		public double R2
+		{
+			get => r2;
+			set
+			{
+				r2 = value;
+				OnPropertyChanged();
+			}
+		}
+		public double Mass
+		{
+			get => mass;
+			set
+			{
+				mass = value;
+				OnPropertyChanged();
+			}
+		}
+		public int CountParticles
+		{
+			get => countParticles;
+			set
+			{
+				countParticles = value;
+				OnPropertyChanged();
+			}
+		}
+		public double DeltaT
+		{
+			get => deltaT;
+
+			set
+			{
+				deltaT = value;
+				OnPropertyChanged(nameof(DeltaT));
+			}
+		}
 		public int TCounts { get; set; } = 200;
 	}
 }
